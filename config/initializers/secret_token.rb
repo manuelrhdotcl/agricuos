dd0ca71023012de90ef5725db720c7240604d344
Manuel Paillafil <manuel@radiohead.cl>
2012-11-05 02:26:01 -0300

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agricuos::Application.config.secret_token = 'fb0aaf065def767569bb057ce2ce94821ad7a16c5676aa25e43dabbbdee903beebd51a05d334f14929b703ffd0998fd072edb6bb1015ba5fd73926d53041cb9b'
